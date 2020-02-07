c6f2737ec167c72c1ce6106172e53a8f9508f200
michael briggs <mjbriggs@icloud.com>
2020-02-07 14:22:27 -0700

    more flexible input

--- d_h.py
+++ d_h.py
@@ -2,47 +2,64 @@ import numpy as np
 from sympy import randprime, isprime, nextprime
 from secrets import token_bytes
 import sys
+import argparse
+
+byte_floor = 63
+byte_len = 8
 
 def mod_exp(a, b, N):
-    b = bin(b)[2:]
-    r = 1
-    for i in range(len(b) - 1, -1, -1):
-        r = (r * a ** int(b[i])) % N
-        a = (a ** 2) % N
-    return r
+  b = bin(b)[2:]
+  r = 1
+  for i in range(len(b) - 1, -1, -1):
+      r = (r * a ** int(b[i])) % N
+      a = (a ** 2) % N
+  return r
 
-def make_dh_values():  
-    print("generating g, a, and p")                              
-    byte_floor = 63
-    bit_floor = byte_floor * 8
-    floor = np.power(2, bit_floor)
-    floor = 2 ** bit_floor
-    ceiling = 2 * floor
-    g = 5
-    a = int.from_bytes(token_bytes(byte_floor), sys.byteorder)
-    p = randprime(floor, ceiling)
-    p_1 = (p - 1) / 2
-    while not isprime(p) and not isprime(p_1):                                
-        p = nextprime(p)
-        p_1 = (p - 1) / 2
+def make_g():
+  return 5
+
+def make_a():
+  byte_floor = 63
+  return int.from_bytes(token_bytes(byte_floor), sys.byteorder)
+
+def make_p():  
+  global byte_floor, byte_len
+  print("\ngenerating g, a, and p")                              
+  bit_floor = byte_floor * byte_len
+  floor = np.power(2, bit_floor)
+  floor = 2 ** bit_floor
+  ceiling = 2 * floor
+  p = randprime(floor, ceiling)
+  p_1 = (p - 1) / 2
+  while not isprime(p) and not isprime(p_1):                                
+      p = nextprime(p)
+      p_1 = (p - 1) / 2
     
-    print("g :", g)
-    print("a :", a)
-    print("p :", p)
-    return g, a, p
+  return p
 if __name__ == '__main__':
-    
-    # g, a, p = make_dh_values()
+  parser = argparse.ArgumentParser(description='Executing diffie-helman with specific parameters')
+  parser.add_argument('-g', type=int, help="Generator for DH", default=None)
+  parser.add_argument('-a', type=int, help="Exponent value", default=None)
+  parser.add_argument('-p', type=int, help="Mod value", default=None)
+  args = parser.parse_args()
+  if not args.g:
+    args.g = make_g()
+  if not args.a:
+    args.a = make_a()
+  if not args.p:
+    args.p = make_p()
+  g = args.g
+  a = args.a 
+  p = args.p
+  
+  print("Values being used ::")
+  print("g :", g)
+  print("a :\n", a)
+  print("p :\n", p)
+  print("\n")
 
-    # print("calculating g ** a mod p")
-    # r = mod_exp(g, a, p)
-    # print(r)
-    # print(pow(g, a, p))
+  # g, a, p = make_dh_values()
 
-    # print("passing off stuff")
-
-    a = 45158020556829761526566715715576392226971171334504306649714622565005857730477671732938482361956767937473240537042068826046002048740622296915051672250725
-    v_in = 374101353318035325384877924330407926628932622145116106468674117878018335148034162010621982500266120905179821783711506834475123467869151157347117496461
-    p = 54233470765699807132834647601416428149958922051760774775293415703747028052046543133967782327341505538314366645343467875720497447641715981000418239354479
-    s = mod_exp(v_in, a, p)
-    print("secret key:", s)
+  r = mod_exp(g, a, p)
+  print("g ** a mod p == \n", r)
+  print("pow(g, a, p) == \n", pow(g, a, p), "\n")
